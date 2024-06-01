8daefd4f51ce5246a3bec7c6f41aecd49aba22a7
narugo1992 <117186571+narugo1992@users.noreply.github.com>
2024-06-01 17:23:38 +0800

    dev(narugo): fix c, ci skip

--- skpick/check/hqimage.py
+++ skpick/check/hqimage.py
@@ -21,14 +21,17 @@ def is_hqimage(file, face_threshold: int = 5000):
         return True
 
     image = load_image(image)
-    if anime_real(image)[0] != 'anime':
-        return False
-    if anime_classify(image)[0] != 'illustration':
-        return False
+    try:
+        if anime_real(image)[0] != 'anime':
+            return False
+        if anime_classify(image)[0] != 'illustration':
+            return False
 
-    for (x0, y0, x1, y1), _, _ in detect_faces(image):
-        area = abs((x1 - x0) * (y1 - y0))
-        if area >= face_threshold:
-            return True
+        for (x0, y0, x1, y1), _, _ in detect_faces(image):
+            area = abs((x1 - x0) * (y1 - y0))
+            if area >= face_threshold:
+                return True
+    except OSError:
+        return False
 
     return False
